59551fffc973350c2b1a60851f4fdcc8ee39fbc4
Michal12351 <michal12351@protonmail.com>
2024-02-22 13:35:36 +0100

    Upload 4

--- src/src/app/Main.java
+++ src/src/app/Main.java
@@ -2,8 +2,6 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Pac-Man");
-        System.out.print("test");
-        System.out.println("test for github");
+        new GameFrame();
     }
 }
