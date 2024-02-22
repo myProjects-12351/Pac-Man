d5224187f533a58bb8a7fec2ae805ccd978fe7bb
Michal12351 <michal12351@protonmail.com>
2024-02-22 14:25:55 +0100

    Upload 5

--- src/src/app/GamePanel.java
+++ src/src/app/GamePanel.java
@@ -3,17 +3,51 @@ package app;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     private static final int BOARD_WIDTH = 600;
     private static final int BOARD_HEIGHT = 600;
+    private static final int FPS = 60;
+
+    Thread thread;
+    PlayManager playManager;
 
     GamePanel(){
         this.setBackground(Color.BLACK);
         this.setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
         System.out.println(this.getSize());
+
+        playManager = new PlayManager();
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+        playManager.draw(g2d);
+    }
+
+    @Override
+    public void run() {
+        double drawInterval = 1000000000 / FPS;  // 1 000 000 000 / fps
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currTime;
+
+        while(thread != null){
+            currTime = System.nanoTime();
+
+            delta += (currTime - lastTime) / drawInterval;
+            lastTime = currTime;
+
+            if(delta > 1){
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+
+    private void update(){
+        playManager.update();
     }
 }
---
+++ src/src/app/PlayManager.java
@@ -0,0 +1,13 @@
+package app;
+
+import java.awt.*;
+
+public class PlayManager {
+    public void draw(Graphics2D g2D){
+
+    }
+
+    public void update(){
+
+    }
+}
