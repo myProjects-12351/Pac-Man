80ca16a1835b416520fe075543a6a91245ed5d20
Michal12351 <michal12351@protonmail.com>
2024-02-22 13:21:01 +0100

    Upload 3

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         System.out.println("Pac-Man");
+        System.out.print("test");
+        System.out.println("test for github");
     }
 }
