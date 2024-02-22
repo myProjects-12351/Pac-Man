085fc575d05daa39f0607d9a54015487e0a155df
Michal12351 <michal12351@protonmail.com>
2024-02-22 14:00:56 +0100

    Upload 5

--- src/src/app/GameFrame.java
+++ src/src/app/GameFrame.java
@@ -6,5 +6,10 @@ public class GameFrame extends JFrame {
     GameFrame() {
         this.add(new GamePanel());
         this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setTitle("PAC MAN");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
     }
 }
--- src/src/app/GamePanel.java
+++ src/src/app/GamePanel.java
@@ -1,9 +1,19 @@
 package app;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GamePanel extends JPanel {
-    GamePanel(){
+    private static final int BOARD_WIDTH = 600;
+    private static final int BOARD_HEIGHT = 600;
 
+    GamePanel(){
+        this.setBackground(Color.BLACK);
+        this.setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
+        System.out.println(this.getSize());
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
     }
 }
